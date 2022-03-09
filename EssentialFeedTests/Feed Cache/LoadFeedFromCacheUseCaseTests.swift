cdec8c4eec6955569460bdf5d28288783caf5f49
Naveen R <naveen.r@rxsling.com>
2022-03-09 20:36:57 +0530

    Load command delivers no images on seven days old cache

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -43,13 +43,25 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
     
     func test_load_deliversCachedImagesOnLessThanSevenDaysOldCache() {
         let feed = uniqueImageFeed()
-        let fixedCurrentDate = Date.init()
-        let lessThanSevenDaysOld = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
-        let (sut, store) = makeSUT(currentDate: { lessThanSevenDaysOld })
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
         execute(sut, toCompleteWith: .success(feed.models), when: {
-            store.completeRetrieval(with: feed.local, timeStamp: lessThanSevenDaysOld)
+            store.completeRetrieval(with: feed.local, timeStamp: lessThanSevenDaysOldTimestamp)
         })
         
+    }
+    
+    func test_load_deliversNoImagesOnSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let sevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        execute(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timeStamp: sevenDaysOldTimestamp)
+        })
     }
     
     //MARK: - Helper
