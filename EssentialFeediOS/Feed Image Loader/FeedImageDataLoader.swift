e7a50bf5ba143894d6fbd830ea781006a4287dc0
Naveen R <naveen.r@rxsling.com>
2022-05-11 20:24:52 +0530

    Amend

---
+++ EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Test 2 on 11/05/22.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
