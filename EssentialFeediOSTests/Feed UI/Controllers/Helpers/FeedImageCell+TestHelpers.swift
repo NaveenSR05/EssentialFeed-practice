8946cd60a39247da6dc6529ec8a78d5ac52f863e
Naveen R <naveen.r@rxsling.com>
2022-05-11 20:24:38 +0530

    Move `FeedImageDataLoader` to its own file also import `EssensialFeediOS` in `EssentialFeediOSTests` test helper classes

--- EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedImageCell+TestHelpers.swift
+++ EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedImageCell+TestHelpers.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import EssentialFeediOS
 
 extension FeedImageCell {
     func simulateRetryAction() {
--- EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeediOSTests/Feed UI/Controllers/Helpers/FeedViewController+TestHelpers.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import EssentialFeediOS
 
 extension FeedViewController {
     func simulateUserInitiatedFeedReload() {
