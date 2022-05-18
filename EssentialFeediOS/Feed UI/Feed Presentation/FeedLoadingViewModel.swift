76e813432f93719c127f61ca586b301a04ccde7e
Naveen R <naveen.r@rxsling.com>
2022-05-18 23:14:33 +0530

    Move ViewModels to separate file

---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Test 2 on 18/05/22.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Test 2 on 18/05/22.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
